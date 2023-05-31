a1ec314d043b60d9654ee7ab9b23931dc649cd4e
Andrea <andita54@hotmail.com>
2023-05-31 12:03:25 +1000

    updating content about variables

---
+++ Session_1/variables.py
@@ -0,0 +1,44 @@
+# first_name = "Asli"
+# last_name ="Yoruk"
+
+
+# #Strings
+# today= "Tuesday"
+# print(today)
+
+# # # Numerical data types- Integer, Float
+# # Integer - whole number
+# parkers_age = 4
+# print(type(parkers_age))
+# print (f"Parker is turning {parkers_age+1} soon!")
+# height = 155
+# print(f"My height is {height/100} in m.")
+
+
+# distance = "5000"
+# print(int(distance)+8)
+# print((distance)+str(8))
+
+# dog_name= input("What's the dog's name?")
+# print(f"Nice to meet you {dog_name}")
+
+# birth_year = input (f"What year you were born?")
+# print(f"You are {2023-int(birth_year)} years old")
+
+# first_number  = input (f"Enter a number: ")
+# second_number  = input (f"Enter another number: ")
+# print(f" Result: {float(first_number) + float(second_number)}")
+
+# first_number  = input (f"Enter a number: ")
+# second_number  = input (f"Enter another number: ")
+# print(f" Multiplication: {float(first_number) * float(second_number)}")
+
+# distance = input(f"How many kilometres: ")
+# distance_float = float(distance)
+# meters = distance_float*1000
+# print(f"the distance in meters is: {int(meters)}")
+# print(f"the distance in metres is : {int((float(distance)*1000))}m in centimetres is  {int(float(distance)*100000)}cm ")
+
+name = input(f"What is your name? ")
+height = input(f"How tall are you (cms)? ")
+print(f"{name} is {height}cms tall ")
